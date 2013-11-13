b47f1b0a40f0a51ef62a435b20eb47a4e26c9941
Christian Lilley <cmlpub@lilfin.net>
2013-11-13 17:20:04 -0500

    changes :-)

--- 3-templates/templates-app.js
+++ 3-templates/templates-app.js
@@ -1,17 +1,25 @@
 angular.module('myApp', ['myResources']);
 
-angular.module('myResources', []).controller('templateController', function($scope){
-    window.bob = $scope.meetupModel = {
-        presenter: "Christian",
-        organizer: "Leigh",
-        yourName: '',
-        templateState: true
-    }
-    $scope.demo= function() {
-        alert("Hello, " + $scope.meetupModel.yourName);
-    }
-});
+angular.module('myResources', [])
+    .controller('templateController', function($scope){
+        window.bob = $scope.meetupModel = {
+            presenter: "Christian",
+            organizer: "Leigh",
+            yourName: '',
+            templateState: true
+        }
+        $scope.demo= function() {
+            alert("Hello, " + $scope.meetupModel.yourName);
+        }
+    });
 
-angular.module('myResources').controller('mainController', function($scope){
-    $scope.mainModel = {template2_path: '../common/template-beta.html'}
-});
+angular.module('myResources')
+    .controller('mainController', function($scope, fileSystem){
+        $scope.mainModel = fileSystem;
+    });
+
+
+angular.module('myResources')
+    .factory('fileSystem', function() {
+        return {template2_path: '../common/template-beta.html'};
+    });
