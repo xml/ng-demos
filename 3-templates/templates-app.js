29988cb87b37cba1a2dbec963487f4bc4dc8dada
Christian Lilley <cmlpub@lilfin.net>
2013-11-15 09:33:46 -0500

    Some comments, and more versions of service definition.

--- 3-templates/templates-app.js
+++ 3-templates/templates-app.js
@@ -1,4 +1,40 @@
+/* You'll see far too many versions of service creation in the Angular docs and 
+    in other examples. To be clear, here are all of them. */ 
+
+/* VERSION 1 OF HOW TO REGISTER A SERVICE (most concise):
+ just call the .service method directly on the module, after creation time. 
+(Alternately: substitute the '.factory' method for '.service'. Same syntax, otherwise) */
+
 angular.module('myApp', ['myResources']);
+
+angular.module('myApp').service('fileSystem', function() {
+        return {template2_path: '../common/template-beta.html'};
+    });
+
+
+/* VERSION 2 OF HOW TO REGISTER A SERVICE:
+ in the config function argument at module creation time
+(Alternately: substitute the '.factory' method for '.service'. Same syntax, otherwise) */
+
+// angular.module('myApp', ['myResources'], function($provide) {
+//     $provide.service('fileSystem', function() {
+//         return {template2_path: '../common/template-beta.html'};
+//     });
+// });
+
+
+/* VERSION 3 OF HOW TO REGISTER A SERVICE:
+   in the module's config method after creation time.
+   (Alternately: substitute the '.factory' method for '.service'. Same syntax, otherwise) */
+
+// angular.module('myApp', ['myResources']);
+
+// angular.module('myApp').config(function($provide) {
+//     $provide.service('fileSystem', function() {
+//         return {template2_path: '../common/template-beta.html'};
+//     });
+// });
+
 
 angular.module('myResources', [])
     .controller('templateController', function($scope){
@@ -19,7 +55,5 @@ angular.module('myResources')
     });
 
 
-angular.module('myResources')
-    .factory('fileSystem', function() {
-        return {template2_path: '../common/template-beta.html'};
-    });
+
+    
