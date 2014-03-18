8984f51d8f982ed31e2f35050cb9dbbfe28dcd1c
Christian Lilley <christian_lilley@mckinsey.com>
2014-03-18 16:50:09 -0400

    updated enter-button directive

--- 6-directives-enter-button/assets/js/class-app-done.js
+++ 6-directives-enter-button/assets/js/class-app-done.js
@@ -10,25 +10,36 @@ angular.module('components', [])
 	.directive('enterButton', function(){
 		return {
 			link: function(scope, element, attrs) {
-				// this example binds a behavior to the mouseenter event
+			// this example binds a behavior to the mouseenter event
+
 				// element.bind("mouseenter", function(){
 				// 	element.addClass(attrs.targetClass); // we're getting the classname from the attribute on the element
 
+				// 	//  Use only for testing what went wrong after switching to a template.
 				// 	// element.addClass('outer');
+				
 				// })
 
 				// element.bind("mouseleave", function(){
 				// 	element.removeClass(attrs.targetClass); // we're getting the classname from the attribute on the element
 				// })
+
 			},
+
 			// controller: function($scope) {
+			// 	console.log('Controller instantiated');
 			// 	$scope.prop1 = {Hello: 'World'};
 			// },
+
+			// restrict: 'EACM',
+
 			// template: '<button class="btn" target-class="btn-danger">Template Button!</button>',
 			// templateUrl: 'assets/partials/button.html',
-			// restrict: 'EACM',
+
 			// replace: true,
-			// scope: {targetClass: '@'},
+
+			// scope: {targetClass: '@',
+			// 		enterButton: '@'}
 
 		}
 	})
@@ -45,15 +56,18 @@ angular.module('components', [])
 1. Basic: Enter
 2. Basic: Leave
 3. Basic w/ Controller
-4. Change to Element
-	4a. No content
-5. So, Restrict: 'EA'
-	6. Template
-	7. Template URL
-8. Not working: no class being added
-	8B. so, check outer: Aha!
-7. So, Replace
-8. Restrict to C & M
+4. Switch to attribute that gets content from template
+5. Now, styles are wacky
+	B. so, check outer: Aha!
+6. So, use: Replace property
+
+5. Template vs templateUrl properties (Ignore the "Type=" for now. )
+6. Change to Element - Result: No content
+7. So, Restrict: 'EA'
+
+10. Add C & M to Restrict (be sure to un-comment the class example)
+11. Now: note that "Type=". To make that work, enable the isolate scope. See how that enables the template type. But...
+	10a. Revert to original attribute. Disable ECM & replace & template. Now, see how it kills "Hello, *World*" by overriding the controller's scope
 
 
 
