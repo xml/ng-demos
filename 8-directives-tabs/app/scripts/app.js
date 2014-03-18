df6fde06288a1d7696d50ac309fff5003a6db5c6
Christian Lilley <christian_lilley@mckinsey.com>
2014-03-18 19:37:32 -0400

    Updates to tabs directives

--- 8-directives-tabs/app/scripts/app.js
+++ 8-directives-tabs/app/scripts/app.js
@@ -1,20 +1,32 @@
 // 'use strict'; 
 //First, we have to create the angular module, because all the other files are going to load and need to attach to it.
-var tabsApp = angular.module('TabsApp', [])
+var tabsApp = angular.module('TabsApp', ['ngRoute'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/home', {
             templateUrl:  'views/home-template.html',
       })
+      .when('/home/level2', {
+            templateUrl:  'views/level2-template.html',
+      })
       .when('/finance', {
             templateUrl:  'views/finance-template.html',
+      })
+      .when('/finance/level2', {
+            templateUrl:  'views/level2-template.html',
       })
       .when('/hr', {                    
             templateUrl: 'views/hr-template.html', // 'views/hr_overview_template.html',      
       })
+      .when('/hr/level2', {
+            templateUrl:  'views/level2-template.html',
+      })
       .when('/quarterly', {                    
             templateUrl:  'views/quarterly_template.html',
         })
+      .when('/quarterly/level2', {
+            templateUrl:  'views/level2-template.html',
+      })
      
       .otherwise({redirectTo: '/home'});
   }]);
--- 8-directives-tabs/app/scripts/directives/activeTab.js
+++ 8-directives-tabs/app/scripts/directives/activeTab.js
@@ -1,27 +1,84 @@
 'use strict';
 
 angular.module('TabsApp')
-  .directive('activeTab', ['$location', function ($location) {
-    return {
-      link: function postLink(scope, element, attrs) {
-        // scope.$on("$routeChangeSuccess", function (event, current, previous) {
-        //     /*  designed for full re-usability at any path, any level, by using data from attrs
-        //         declare like this: <li class="nav_tab"><a href="#/home" active-tab="1">HOME</a></li> */
+    .directive('detectActiveTab', ['$location',
+        function($location) {
+            return {
+                link: function postLink(scope, element, attrs) {
+                    scope.$on("$routeChangeSuccess", function(event, current, previous) {
+                        /*  
+                        Designed for full re-usability at any path, any level, by using data from attrs.
+                        Declare like this: <li class="nav_tab"><a href="#/home" active-tab="1">HOME</a></li> 
+                        */
 
-        //     // this var grabs the tab-level off the attribute, or defaults to 1
-        //     var pathLevel = attrs.activeTab || 1,
-        //     // this var finds what the path is at the level specified
-        //         pathToCheck = $location.path().split('/')[pathLevel],
-        //     // this var finds grabs the same level of the href attribute
-        //         tabLink = attrs.href.split('/')[pathLevel];
-        //     // now compare the two:
-        //     if (pathToCheck === tabLink) {
-        //       element.addClass("active");
-        //     }
-        //     else {
-        //       element.removeClass("active");
-        //     }
-        // });
-      }
-    };
-  }]);
+                        // This var grabs the tab-level off the attribute, or defaults to 1
+                        var pathLevel = attrs.detectActiveTab || 1,
+                            // This var finds what the path is at the level specified
+                            pathToCheck = $location.path().split('/')[pathLevel] || "current $location.path doesn't reach this level",
+                            // This var finds grabs the same level of the href attribute
+                            tabLink = attrs.href.split('/')[pathLevel] || "href doesn't include this level";
+                        console.log(pathToCheck);
+                        console.log(tabLink);
+                        // Above, we use the logical 'or' operator to provide a default value in cases 
+                        // where 'undefined' would otherwise be returned.
+                        // This prevents cases where undefined===undefined, possibly causing multiple tabs to be 'active'.
+
+                        // now compare the two:
+                        if (pathToCheck === tabLink) {
+                            element.addClass("active");
+                        } else {
+                            element.removeClass("active");
+                        }
+                    });
+                }
+            };
+        }
+    ]);
+
+angular.module('TabsApp')
+    .directive('findActiveTab', ['$location',
+        function($location) {
+            return {
+                link: function postLink(scope, element, attrs) {
+                    scope.$on("$routeChangeSuccess", function(event, current, previous) {
+                        /*  designed for full re-usability at any path, any level, by using data from attrs
+                            declare like this: 
+                              <nav show-active-tab="1"> 
+                                <a href="#/home">HOME</a>
+                              </nav> 
+
+                        */
+
+                        // this var grabs the tab-level off the attribute, or defaults to 1
+                        var pathLevel = attrs.findActiveTab || 1,
+                            // this var finds what the $location.path is at the level specified
+                            pathToCheck = $location.path().split('/')[pathLevel] || "current $location.path doesn't reach this level";
+
+                        // now, iterate the children. Ideally, child elements should all be <a>'s. 
+                        angular.forEach(element.children(), function(item) {
+                            var specimen = angular.element(item),
+                                // find each item's matching level on the href attribute
+                                tabLink = specimen.attr('href').split('/')[pathLevel] || "href doesn't include this level";
+                            // now compare the item to the current $location:
+                            if (pathToCheck === tabLink) {
+                                specimen.addClass("active");
+                            } else {
+                                specimen.removeClass("active");
+                            }
+                        })
+                    });
+                }
+            };
+        }
+    ]);
+
+
+
+/*
+1. Behavior without, then with 'detect' directive
+2. Note declarative params, just like clock.
+3. Try /anything/level2
+4. Demo 'find' version
+
+
+*/
