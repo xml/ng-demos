8301d17b558aa7b533a55b94584593e3f736efae
Christian Lilley <christian_lilley@mckinsey.com>
2014-05-22 18:42:01 -0400

    disable hg-repeat at the outset

--- 5-ng-repeat/ngrepeat-app.js
+++ 5-ng-repeat/ngrepeat-app.js
@@ -1,24 +1,25 @@
 angular.module('myApp', []).controller('repeatController', function($scope){
-    $scope.gurus = [
-        {
-            'name': "Christian Lilley",
-            'age': 39,
-            'specialty': "Talking Too Much about Cool Stuff",
-            'img': "images/cml.jpg"
-        },
-        {
-            'name': "Leigh Frampton",
-            'age': 19,
-            'specialty': "Organizing Cool Meetups about Cool Stuff",
-            'img': "images/lf.jpg"
-        },
-        {
-            'name': "Misko Hevery",
-            'age': 1000,
-            'specialty': "Awesomely SuperHeroic MVW Frameworks",
-            'img': "images/mh.png"
-        }
-    ]
+    /* STEP 0: ENABLE THE DATA TO POPULATE THE REPEAT */
+    // $scope.gurus = [
+    //     {
+    //         'name': "Christian Lilley",
+    //         'age': 39,
+    //         'specialty': "Talking Too Much about Cool Stuff",
+    //         'img': "images/cml.jpg"
+    //     },
+    //     {
+    //         'name': "Leigh Frampton",
+    //         'age': 19,
+    //         'specialty': "Organizing Cool Meetups about Cool Stuff",
+    //         'img': "images/lf.jpg"
+    //     },
+    //     {
+    //         'name': "Misko Hevery",
+    //         'age': 1000,
+    //         'specialty': "Awesomely SuperHeroic MVW Frameworks",
+    //         'img': "images/mh.png"
+    //     }
+    // ]
     $scope.config = {
         sorter: "name",
         options: ["name", "age", "specialty"],
