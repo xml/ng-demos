e40da2dcb35659e2d33ce2a2a448f13b52880d06
Christian Lilley <christian_lilley@mckinsey.com>
2014-05-22 16:32:56 -0400

    more updates & comments for 4, 5, 6

--- 4-routing/routing-app.js
+++ 4-routing/routing-app.js
@@ -6,15 +6,16 @@ angular.module('myApp', ['myResources'])
             .otherwise({redirectTo: '/alpha'});
     });
 
-
+// STEP 1: Note how routing works, with templates and controllers
+// STEP 2: Note the nested controllers because they're assigned both in template and in routing. Disable in template.
+// STEP 3: Note controllers are INSTANCES, and non-persistent, unlike services, as you toggle back and forth between two views that share the same controller *constructor*. 
 
 
 angular.module('myResources', []).controller('templateController', function($scope){
     window.bob = $scope.meetupModel = {
         presenter: "Christian",
-        organizer: "Leigh",
-        yourName: '',
-        templateState: true
+        organizer: "Tom",
+        yourName: ''
     }
     $scope.demo = function() {
         alert("Hello, " + $scope.meetupModel.yourName);
--- 6-directives-enter-button/enter-button.js
+++ 6-directives-enter-button/enter-button.js
@@ -12,35 +12,54 @@ angular.module('components', [])
 			link: function(scope, element, attrs) {
 			// this example binds a behavior to the mouseenter event
 
+				// STEP 1: ENABLE A BASIC ENTER HANDLER: note how we use declarative classnames for re-usability
 				// element.bind("mouseenter", function(){
-				// 	element.addClass(attrs.targetClass); // we're getting the classname from the attribute on the element
+				// 	// we're getting the classname from the attribute on the element
+				// 	element.addClass(attrs.targetClass); 
 
-				// 	//  Use only for testing what went wrong after switching to a template.
+				// 	//  STEP 4B: test what went wrong after switching to a template.
 				// 	// element.addClass('outer');
-				
 				// })
 
+				// STEP 2: ENABLE A BASIC LEAVE HANDLER
 				// element.bind("mouseleave", function(){
-				// 	element.removeClass(attrs.targetClass); // we're getting the classname from the attribute on the element
+				// 	// we're getting the classname from the attribute on the element
+				// 	element.removeClass(attrs.targetClass);
 				// })
 
 			},
 
+			// STEP 3: ENABLE THE DIRECTIVE'S OWN CONTROLLER, TO CARRY ITS OWN DATA PAYLOAD
+			// (directives can be mini-applications in their own little world)
 			// controller: function($scope) {
 			// 	console.log('Controller instantiated');
 			// 	$scope.prop1 = {Hello: 'World'};
 			// },
 
-			// restrict: 'EACM',
-
+			// STEP 4: GO OUT TO THE TEMPLATE AND SWITCH TO BYO TEMPLATE, BUT NOTE ODDITY...
 			// template: '<button class="btn" target-class="btn-danger">Template Button!</button>',
+
+			// STEP 5: SWITCH TO A FILE-BASED TEMPLATE, NOT INLINE
 			// templateUrl: 'button.html',
 
+
+			// STEP 4C: NOW THAT WE KNOW WHAT'S WRONG, FIX IT WITH `replace:`
 			// replace: true,
 
-			// scope: {targetClass: '@',
-			// 		enterButton: '@'}
+			// STEP 6: GO OUT TO TEMPLATE AND SWITCH INVOCATION STYLE TO ELEMENT, AND SEE IF IT WORKS...
 
+			// STEP 7: IT DOESN'T WORK. BECAUSE WE NEED TO PROVIDE THE restrict: PROPERTY
+			// restrict: 'E',
+
+			// STEP 8: SHOW BOTH OPTIONS, THEN MAKE restrict: 'EA' TO ALLOW BOTH TO WORK
+
+			// STEP 9: restrict: 'EACM' - ENABLE CLASS BUTTON, TOO, AND NOTE THE ONE BELOW IT DOESN'T NEED OUR HELP
+
+			// STEP 10: ENABLE ISOLATE SCOPE. Note that you lose the attr on an element declaration.
+			// scope: {targetClass: '@',
+					// enterButton: '@'}
+
+			// STEP 11: REVERT TO ORIGINAL ATTRIBUTE DIRECTIVE, DISABLE REPLACE, RESTRICT & TEMPLATE, BUT KEEP ISOLATE SCOPE. Note that the properties on the controller aren't accessible. They're being overridden by the isolate scope. Disable isolate scope, and they come back.
 		}
 	})
 
